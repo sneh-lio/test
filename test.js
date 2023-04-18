4a4a8e7ccdecbfc5de23cc72ce0b7ec3707ff006
Sneh Sagar <90624704+sneh-lio@users.noreply.github.com>
2023-04-18 12:05:03 +0530

    Update test.js

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ else if (number > 1) {
         }
     }
 
-    if (isPrime) {
+    if (!isPrime) {
         console.log(`${number} is a prime number`);
     } else {
         console.log(`${number} is a not prime number`);
